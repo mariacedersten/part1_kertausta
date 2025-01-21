e581b15885ec5fd2dd87da0d93b4696767f4d734
mariacedersten <s2401554@edu.bc.fi>
2025-01-21 14:10:20 +0200

    Tehty

---
+++ Kertaus1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System;
+
+internal class Program
+{
+    private const int MaximumNumberOfCalls = 500;
+
+    private static void Main(string[] args)
+    {
+        // Try to parse first argument as number
+        int.TryParse(args.FirstOrDefault("0"), out int num);
+
+        Console.WriteLine($"starting with arg: {num}");
+
+        num++;
+
+        if (num > MaximumNumberOfCalls)
+        {
+            Console.WriteLine($"Exceeded MaximumNumberOfCalls {MaximumNumberOfCalls}");
+            Environment.Exit(0);
+        }
+
+        using (var p = new Process())
+        {
+            // Get current program executable
+            p.StartInfo.FileName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            // Remove .dll from the end of the file path (macos stuff)
+            p.StartInfo.FileName = p.StartInfo.FileName.Substring(0, p.StartInfo.FileName.Length - 4);
+
+            p.StartInfo.Arguments = num.ToString();
+            p.StartInfo.UseShellExecute = true; // needed in macos
+            p.Start();
+
+            p.WaitForExit(); // Important
+        }
+
+        Console.WriteLine("Ending -- " + num.ToString());
+    }
+}
---
+++ Kertaus2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace BC_ohjelmointi
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Käytä Microsoftin nugetteja
+            // ja lue konfiguraatio tedosto (appsettings.json)
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            // Käytä Serilog nugettia konfeguroidaksesi
+            // Serilogin käyttämän appsettings.json-tiedostosta
+            // luettua konfeguraatiota
+            var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+            // Esimerkki
+            logger.Error("Paha error");
+            logger.Warning("Varoitus!");
+            logger.Information("Hello, world!");
+
+        }
+
+
+    }
+
+}
